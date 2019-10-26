34cd894b05860b5b9733054b172510b269b157bf
Erik van Oosten <e.vanoosten@grons.nl>
2019-10-26 09:28:31 +0200

    Iterator.takeUntilException

--- src/main/scala/scala/collection/decorators/IteratorDecorator.scala
+++ src/main/scala/scala/collection/decorators/IteratorDecorator.scala
@@ -2,6 +2,7 @@ package scala.collection
 package decorators
 
 import scala.annotation.tailrec
+import scala.util.control.NonFatal
 
 /** Enriches Iterator with additional methods.
   *
@@ -225,4 +226,59 @@ class IteratorDecorator[A](val `this`: Iterator[A]) extends AnyVal {
         }
       }
     }
+
+  /** Gives elements from the source iterator until the source iterator ends or throws an exception.
+    *
+    * @return an iterator that takes items until the source iterator ends or throws an exception
+    * @note   Reuse: $consumesAndProducesIterator
+    */
+  def takeUntilException: Iterator[A] = {
+    new AbstractIterator[A] {
+      private val wrapped = `this`.buffered
+
+      override def hasNext: Boolean = {
+        try {
+          val n = wrapped.hasNext
+          // By already invoking `head` (and therefore also `next` on `this`),
+          // we are sure that `wrapped.next` will not throw when it is used from
+          // `next`.
+          if (n) wrapped.head
+          n
+        } catch {
+          case NonFatal(t) => false
+        }
+      }
+
+      override def next(): A = wrapped.next
+    }
+  }
+
+  /** Gives elements from the source iterator until the source iterator ends or throws an exception.
+    *
+    * @param  exceptionCaught a callback invoked from `hasNext` when the source iterator throws an exception
+    * @return an iterator that takes items until the wrapped iterator ends or throws an exception
+    * @note   Reuse: $consumesAndProducesIterator
+    */
+  def takeUntilException(exceptionCaught: Throwable => Unit): Iterator[A] = {
+    new AbstractIterator[A] {
+      private val wrapped = `this`.buffered
+
+      override def hasNext: Boolean = {
+        try {
+          val n = wrapped.hasNext
+          // By already invoking `head` (and therefore also `next` on `this`),
+          // we are sure that `wrapped.next` will not throw when it is used from
+          // `next`.
+          if (n) wrapped.head
+          n
+        } catch {
+          case NonFatal(t) =>
+            exceptionCaught(t)
+            false
+        }
+      }
+
+      override def next(): A = wrapped.next
+    }
+  }
 }
